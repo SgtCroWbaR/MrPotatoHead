465069e40a937efbde14bb18d95679b19d7423b8
SgtCroWbar <sgt.crowbar@gmail.com>
2021-04-16 08:24:15 +0200

    ported to win

---
+++ cmake-build-debug/CMakeFiles/Potato.dir/DependInfo.cmake
@@ -0,0 +1,32 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/Vessely/Desktop/MrPotatoHead/src/main.cpp" "C:/Users/Vessely/Desktop/MrPotatoHead/cmake-build-debug/CMakeFiles/Potato.dir/src/main.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "MSVC")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "IMGUI_IMPL_OPENGL_LOADER_GLAD"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "configuration"
+  "../include"
+  "../libs/glad/include"
+  "../libs/imgui/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "C:/Users/Vessely/Desktop/MrPotatoHead/cmake-build-debug/libs/glad/CMakeFiles/glad.dir/DependInfo.cmake"
+  "C:/Users/Vessely/Desktop/MrPotatoHead/cmake-build-debug/CMakeFiles/STB_IMAGE.dir/DependInfo.cmake"
+  "C:/Users/Vessely/Desktop/MrPotatoHead/cmake-build-debug/libs/imgui/CMakeFiles/imgui.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/Potato.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "../bin/Potato.exe"
+  "../bin/Potato.exe.manifest"
+  "../bin/Potato.pdb"
+  "CMakeFiles/Potato.dir/src/main.cpp.obj"
+  "Potato.lib"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Potato.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
